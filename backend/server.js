7d6214dfbd0653d269d2a3fc879b7e9c84dc2a98
zablackafro <zahidabdikadir12@gmail.com>
2022-04-01 16:12:16 -0500

    added basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Ticket API' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`))
